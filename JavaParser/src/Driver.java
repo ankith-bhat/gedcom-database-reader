3e9c419efdde848026a8c22af8e19c03f61618f3
Ankith Bhat <ankithhbhat@gmail.com>
2019-12-26 12:09:55 -0600

    Started JavaParser

---
+++ JavaParser/src/Driver.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2019 Ankith Bhat, Amith Bhat
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import java.io.*;
+
+public class Driver {
+
+    public Driver() {
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        File file = new File("555SAMPLE.GED");
+
+        // todo try-catch
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        // todo try-catch
+        String st;
+        while ((st = br.readLine()) != null)
+            System.out.println(st);
+    }
+}
+
